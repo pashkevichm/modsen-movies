0cd23077be2e9da9eb9ee4b62b6a0c2382e7dc96
Maxim Pashkevich <maxim.pashkevich@modsen-software.com>
2023-06-01 08:31:42 +0300

    feat: configured android splash screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { SafeAreaView, Text } from 'react-native';
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
-	// useEffect(() => {
-	// 	SplashScreen.hide();
-	// }, []);
+	useEffect(() => {
+		SplashScreen.hide();
+	}, []);
 
 	return (
 		<SafeAreaView>
